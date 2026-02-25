430af83ac0826c7bf14e82a5d95916b799de1eaa
fmf6544 <fmf6544@nyu.edu>
2026-02-25 12:10:12 -0500

    Halfway done

--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -21,12 +21,57 @@ class Customer < ApplicationRecord
   #  You should NOT need to call Ruby library functions for sorting, filtering, etc.
 
   def self.any_candice
-    # YOUR CODE HERE to return all customer(s) whose first name is Candice
-    # probably something like:  Customer.where(....)
+    Customer.where(first: "Candice")
   end
 
   def self.with_valid_email
     # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
+    Customer.where("email LIKE '%@%'")
   end
-  # etc. - see README.md for more details
+
+  def self.with_dot_org_email
+    Customer.where("email like '%@%.org'")
+  end
+
+  def self.with_invalid_email
+    Customer.where.not("email LIKE '%@%'")
+  end
+
+  def self.with_blank_email
+    Customer.where(email: ["", nil])
+  end
+
+  def self.born_before_1980
+    Customer.where("birthdate < '1980-01-01'")
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    Customer.where("birthdate < '1980-01-01' AND email like '%@%'")
+  end
+
+  def self.last_names_starting_with_b
+
+  end
+
+  def self.twenty_youngest
+
+  end
+
+  # Methods to update table
+  def self.update_gussie_murray_birthdate
+
+  end
+
+  def self.change_all_invalid_emails_to_blank
+
+  end
+
+  def self.delete_meggie_herman
+
+  end
+
+  def self.delete_everyone_born_before_1978
+
+  end
+
 end
--- spec/activerecord_practice_spec.rb
+++ spec/activerecord_practice_spec.rb
@@ -39,31 +39,31 @@ describe 'ActiveRecord practice' do
         expect(Customer).to receive(:where).at_least(:once).and_call_original
       end
 
-      xspecify 'anyone with first name Candice' do
+      specify 'anyone with first name Candice' do
         check Customer.any_candice, [24]
       end
 
-      xspecify 'with valid email (email addr contains "@") (HINT: look up SQL LIKE operator)' do
+      specify 'with valid email (email addr contains "@") (HINT: look up SQL LIKE operator)' do
         check Customer.with_valid_email, [1, 2, 4, 5, 7, 8, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 23, 26, 29, 30]
       end
 
-      xspecify 'with .org emails' do
+      specify 'with .org emails' do
         check Customer.with_dot_org_email, [5, 7, 8, 12, 23, 26, 29]
       end
 
-      xspecify 'with invalid but nonblank email (does not contain "@")' do
+      specify 'with invalid but nonblank email (does not contain "@")' do
         check Customer.with_invalid_email, [3, 6, 9, 16, 22, 25, 27, 28]
       end
 
-      xspecify 'with blank email' do
+      specify 'with blank email' do
         check  Customer.with_blank_email, [21, 24]
       end
 
-      xspecify 'born before 1 Jan 1980' do
+      specify 'born before 1 Jan 1980' do
         check Customer.born_before_1980, [3, 8, 9, 11, 15, 16, 17, 19, 20, 24, 25, 27]
       end
 
-      xspecify 'with valid email AND born before 1/1/1980' do
+      specify 'with valid email AND born before 1/1/1980' do
         check Customer.with_valid_email_and_born_before_1980, [8, 11, 15, 17, 19, 20]
       end
 
